5ba1579b2c6b7950ebedaede8e1048952c8706c8
NguyenNam219 <npn210906@gmail.com>
2025-08-11 21:30:42 +0700

    update

--- data/exams/de1.js
+++ data/exams/de1.js
@@ -2,7 +2,7 @@ export const de1Exam = {
     id: 14, // ID phải khác với các đề đã có
     title: "SSL101c_-_SP_2025_-_RE_3806",
     description: "Kiểm tra kiến thức HTML, CSS, JavaScript",
-    totalQuestions: 50, // Số câu hỏi thực tế
+    totalQuestions: 60, // Số câu hỏi thực tế
     questions: [
  {
         id: 1,
